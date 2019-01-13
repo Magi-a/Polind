e3e77e4e6dcff647a46f182be66d3915ced5c3a7
Nik <nik-pavlovich@mail.ru>
2019-01-13 03:05:28 +0300

    Add final version of site

---
+++ js/common.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    let menuOpen = false;
+
+    $('.menu-background').fadeOut(1);
+
+    $(window).on("scroll", function() {
+
+        if(document.body.clientWidth < 992 && !menuOpen)
+        {
+            if($(window).scrollTop()) {
+                $('.header').addClass('dark-header');
+            }
+            else {
+                $('.header').removeClass('dark-header');
+            }
+        }
+    });
+
+    $('#humburger-in').on('click', function () {
+        $('.hamburger').toggleClass('open');
+        $('.menu-background').fadeToggle(400);
+        $('.drop-menu').toggleClass('drop-menu-open');
+        
+        if(menuOpen){
+            menuOpen = false;
+        } else {
+            menuOpen = true;
+        }
+        
+        if(menuOpen) {
+            $('.header').removeClass('dark-header');
+        }
+        else {
+            $('.header').addClass('dark-header');
+        }
+
+        if(!($(window).scrollTop())) {
+            $('.header').removeClass('dark-header');
+        }
+    });
+
+});
