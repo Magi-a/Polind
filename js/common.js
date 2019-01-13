9b4489bfd8da64f4aa7e98e88f2306a083ee1cf8
Nik <nik-pavlovich@mail.ru>
2019-01-13 03:16:22 +0300

    hot fix

--- js/common.js
+++ js/common.js
@@ -40,4 +40,22 @@ $(document).ready(function() {
         }
     });
 
+    $("a").on('click', function(event) {
+        event.preventDefault();
+        /*
+		if (this.hash !== "") {
+		  event.preventDefault();
+
+		  var hash = this.hash;
+
+		  $('html, body').animate({
+			scrollTop: $(hash).offset().top
+		  }, 800, function(){
+
+			window.location.hash = hash;
+
+		  });
+        } */
+  	});
+
 });
